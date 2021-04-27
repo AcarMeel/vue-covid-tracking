8d36e6791cf8556dc75bea83712519980d27b244
acarmeel <acarmeel@MacBook-Pro-de-acarmeel.local>
2021-04-26 20:26:26 -0600

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+  <header class="text-center bg-blue-800 text-white p-4 mb-10">
+    <div class="text-3xl md:text-5xl font-bold mb-3"><i class="fa fa-viruses text-green-300"></i>Covid 19 Tracker</div>
+    <p class="">
+      API by <a href="https://covid19api.com/" class="text-blue-300" target="_blank">
+      Covid 19 API</a>
+    </p>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
